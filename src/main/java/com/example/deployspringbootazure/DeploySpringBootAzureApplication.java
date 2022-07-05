baa0a84e96aac65bdece3e2892c81ff199df9463
lamdangtung <lam.dt26042000@gmail.com>
2022-07-05 08:53:02 +0700

    Initial commit

---
+++ src/main/java/com/example/deployspringbootazure/DeploySpringBootAzureApplication.java
@@ -0,0 +1,13 @@
+package com.example.deployspringbootazure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploySpringBootAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeploySpringBootAzureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/deployspringbootazure/DeploySpringBootAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deployspringbootazure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploySpringBootAzureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
